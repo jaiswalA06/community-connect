9e63290d4652dd233f74f09193d0be6d08b9cd16
kripaa-156 <kripatawri105@gmail.com>
2026-01-05 15:14:15 +0530

    Add subtitle describing community resources and volunteering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import MapView from "./components/MapView";
-
 function App() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>CommunityConnect</h1>
+
+      <div style={{ padding: "10px", textAlign: "center" }}>
+        <p>Discover nearby community resources and volunteer opportunities</p>
+      </div>
+
       <MapView />
     </div>
   );
 }
-
 export default App;
-;
-
